56a12f55bf62517d8fc2a142eeded45ecbb9def0
Frans Yohanes <fransyohanes73@gmail.com>
2023-07-04 00:24:24 +0700

    login screen user

--- trashure_code_flutter/lib/Frans_Code/Log-in_Sign-In_Screen.dart
+++ trashure_code_flutter/lib/Frans_Code/Log-in_Sign-In_Screen.dart
@@ -16,59 +16,61 @@ class _Login_SignState extends State<Login_Sign> {
     return Scaffold(
       backgroundColor: Colors.trashure,
       // appBar: AppBar(title: Text("trashure")),
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-                // padding: EdgeInsets.only(top: 100),
-                // height: 229,
-                // color: Colors.deepOrangeAccent,
-                child: Image.asset(
-              "assets/logo final.jpg",
-              cacheHeight: 229,
-            )),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                      onPressed: () {
-                        setState(() {
-                          status = "login";
-                          print(status);
-                        });
-                      },
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                            color: Color.fromRGBO(153, 235, 60, 100),
-                            decoration: status == "login"
-                                ? TextDecoration.underline
-                                : null,
-                            fontSize: 15),
-                      )),
-                  TextButton(
-                      onPressed: () {
-                        setState(() {
-                          status = "sign-up";
-                          print(status);
-                        });
-                      },
-                      child: Text(
-                        "Sign-Up",
-                        style: TextStyle(
-                            color: Color.fromRGBO(153, 235, 60, 100),
-                            decoration: status == "sign-up"
-                                ? TextDecoration.underline
-                                : null,
-                            fontSize: 15),
-                      ))
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  padding: EdgeInsets.only(top: 100),
+                  // height: 229,
+                  // color: Colors.deepOrangeAccent,
+                  child: Image.asset(
+                    "assets/logo final.jpg",
+                    cacheHeight: 229,
+                  )),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            status = "login";
+                            print(status);
+                          });
+                        },
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                              color: Color.fromRGBO(153, 235, 60, 100),
+                              decoration: status == "login"
+                                  ? TextDecoration.underline
+                                  : null,
+                              fontSize: 20),
+                        )),
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            status = "sign-up";
+                            print(status);
+                          });
+                        },
+                        child: Text(
+                          "Sign-Up",
+                          style: TextStyle(
+                              color: Color.fromRGBO(153, 235, 60, 100),
+                              decoration: status == "sign-up"
+                                  ? TextDecoration.underline
+                                  : null,
+                              fontSize: 20),
+                        ))
+                  ],
+                ),
               ),
-            ),
-            Container(child: status == "login" ? Login_Form() : Sign_Form()),
-          ]),
+              Container(child: status == "login" ? Login_Form() : Sign_Form()),
+            ]),
+      ),
     );
   }
 }
--- trashure_code_flutter/lib/main.dart
+++ trashure_code_flutter/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:trashure_code_flutter/Frans_Code/Log-in_Sign-In_Screen.dart';
 import 'package:trashure_code_flutter/Frans_Code/Splash_Screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,17 +17,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.trashure,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.trashure,
+          textTheme: GoogleFonts.ubuntuTextTheme()),
       home:
           // const MyHomePage(title: 'Flutter Demo Home Page'),
           // const Login_Sign(),
